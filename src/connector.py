d86f34cfe59be3255c41e739cfeaa512b41dc939
TukangLas21 <19623160@std.stei.itb.ac.id>
2025-06-13 20:37:20 +0700

    feat: connector class

---
+++ src/connector.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+class Connector:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+        self.cursor = None
+        
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            if self.connection.is_connected():
+                self.cursor = self.connection.cursor(True)
+                print("Connection successful")
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            self.connection = None
+
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            print("Connection closed")
+            
+    def get_applicant_profile(self, applicant_id):
+        if not self.connection or not self.connection.is_connected():
+            print("No active connection")
+            return None
+        try:
+            query = "SELECT * FROM ApplicantProfile WHERE applicant_id = %s"
+            self.cursor.execute(query, (applicant_id,))
+            return self.cursor.fetchone()
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
+    
+    
+        
+            
+        
